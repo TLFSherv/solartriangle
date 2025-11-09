10097c135875f824b3e47c087ea0b9595d56a828
Sherwin Burgess <sherwinburgess@gmail.com>
2025-11-09 11:27:23 -0400

    added styling for small devices

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image"
 import Link from "next/link";
 import Button from "./components/Button";
 import graph from "../public/graph-sm.svg"
+import graph_lg from "../public/graph-lg.png"
 import dataImg from "../public/data-sm.svg"
 
 export default function Home() {
   return (
-    <div className="flex-1 flex flex-col space-y-40 my-20 mx-auto">
+    <div className="flex-1 flex flex-col space-y-30 sm:space-y-40 my-20 mx-auto">
       {/* Hero Section */}
       <div className='space-y-14 mx-8'>
         <h1 className="text-5xl/14 sm:text-7xl/24 text-center tracking-wider font-[Space_Grotesk] text-[#F0662A]">
@@ -20,26 +21,29 @@ export default function Home() {
         </div>
       </div>
       {/* Benefits Section */}
-      <div className='flex flex-col-reverse sm:flex-row mx-8 gap-6 justify-evenly items-center'>
-        <ul className="space-y-2 text-md sm:text-lg font-[Inter] font-light list-disc">
+      <div className='flex flex-col-reverse sm:flex-row mx-8 gap-12 sm:gap-6 justify-evenly items-center'>
+        <ul className="space-y-4 text-xl font-[Inter] font-light list-disc">
           <li>Accurately predict future power generation</li>
           <li>Optimise your solar system</li>
           <li>Keep a history of past performance</li>
           <li>Analyse future performance</li>
         </ul>
-        <Image src={graph} alt="graph" />
+        <picture>
+          <source media="(min-width:650px)" srcSet={graph} />
+          <Image src={graph_lg} alt="graph" />
+        </picture>
       </div>
       {/* Summary section */}
       <div className='flex flex-col sm:flex-row items-center mx-8 gap-6'>
         <Image src={dataImg} alt="data" />
         <div className='text-lg font-[Inter] font-light flex-1 space-y-6'>
-          <p>
+          <p className="px-8 text-xl">
             The solar calculator uses the most accurate real
             time weather data, location data and panel performance
             data to give an accurate forecast of future power
             generation of your current or future solar system.
           </p>
-          <div className="text-lg font-light font-[Inter]">
+          <div className="flex justify-center text-lg font-light font-[Inter]">
             <Link href={"/"}><Button text="Learn more" /></Link>
           </div>
         </div>
