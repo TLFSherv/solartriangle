fe7006db38fb2bf228e21c52ce093ab21f673fa5
Sherwin Burgess <sherwinburgess@gmail.com>
2025-11-30 17:02:13 -0400

    make title and paragraph disappear when card is clicked

--- app/learn/(home)/components/TopicCard.tsx
+++ app/learn/(home)/components/TopicCard.tsx
@@ -1,20 +1,16 @@
 "use client"
-import React, { useState } from "react"
 
-export default function TopicCard({ children, title }: { children: React.ReactNode, title: string }) {
-    const [isHidden, setIsHidden] = useState(true);
-    const clickHandler = () => {
-        setIsHidden(!isHidden);
-    }
+export default function TopicCard({ children, title, isActive }: { children: React.ReactNode, title: string, isActive: boolean }) {
+
     return (
         <div className="border-2 border-[#DD6B19] rounded-lg font-[space_grotesk] font-light">
-            <div className="p-4 cursor-pointer" onClick={clickHandler}>
+            <div className="p-4 cursor-pointer">
                 <h2 className="text-xl pb-1">
-                    <span className="pr-2">{isHidden ? "+" : "-"}</span>
+                    <span className="pr-2">{isActive ? "-" : "+"}</span>
                     {title}
                 </h2>
             </div>
-            {!isHidden && <div className="bg-[#D9D9D9] text-black text-sm list-decimal list-inside space-y-6 p-4 rounded-b-md">
+            {isActive && <div className="bg-[#D9D9D9] text-black text-sm list-decimal list-inside space-y-6 p-4 rounded-b-md">
                 {children}
             </div>
             }
--- app/learn/(home)/page.tsx
+++ app/learn/(home)/page.tsx
@@ -1,37 +1,54 @@
+"use client"
+import React, { useState } from "react";
 import Link from "next/link"
 import TopicCard from "./components/TopicCard";
 
 export default function Learn() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const isHidden = activeIndex !== 0;
     return (
         <div className="m-6 space-y-8">
-            <h1 className="text-[64px] tracking-wide font-[Lexend_Deca] text-center">
-                Learn
-                <span className="text-[#F0662A]">.</span>
-            </h1>
-            <p className="font-[Inter] font-light text-lg sm:text-xl text-center">
-                This is a distillation of the notes I’ve made during my journey to learn about solar panels.
-                I hope the way I’ve organised and explained the content helps you understand this fascinating topic like it did for me.
-                Welcome
-            </p>
+            {
+                !isHidden &&
+                <div>
+                    <h1 className="text-[64px] tracking-wide font-[Lexend_Deca] text-center">
+                        Learn
+                        <span className="text-[#F0662A]">.</span>
+                    </h1>
+                    <p className="font-[Inter] font-light text-lg sm:text-xl text-center">
+                        This is a distillation of the notes I’ve made during my journey to learn about solar panels.
+                        I hope the way I’ve organised and explained the content helps you understand this fascinating topic like it did for me.
+                        Welcome
+                    </p>
+                </div>
+            }
             <div className="space-y-4">
-                <TopicCard title={"Panel Tilt"}>
-                    <section>
-                        <h3>1. Latitude and longitude</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam dignissimos quam! Praesentium harum itaque ut labore dicta et ea accusantium voluptatem, veritatis tenetur culpa? Impedit, natus? Odio, tempora unde.</p>
-                    </section>
-                    <section>
-                        <h3>2. Earth</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam dignissimos quam! Praesentium harum itaque ut labore dicta et ea accusantium voluptatem, veritatis tenetur culpa? Impedit, natus? Odio, tempora unde.</p>
-                    </section>
-                    <section>
-                        <h3>3. Seasons</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam dignissimos quam! Praesentium harum itaque ut labore dicta et ea accusantium voluptatem, veritatis tenetur culpa? Impedit, natus? Odio, tempora unde.</p>
-                    </section>
-                </TopicCard>
-                <TopicCard title={"Azimuth"}>
-                    <section>
-                    </section>
-                </TopicCard>
+                <div onClick={() => setActiveIndex(activeIndex === 1 ? 0 : 1)}>
+                    <TopicCard
+                        isActive={activeIndex === 1}
+                        title={"Panel Tilt"}>
+                        <section>
+                            <h3>1. Latitude and longitude</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam dignissimos quam! Praesentium harum itaque ut labore dicta et ea accusantium voluptatem, veritatis tenetur culpa? Impedit, natus? Odio, tempora unde.</p>
+                        </section>
+                        <section>
+                            <h3>2. Earth</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam dignissimos quam! Praesentium harum itaque ut labore dicta et ea accusantium voluptatem, veritatis tenetur culpa? Impedit, natus? Odio, tempora unde.</p>
+                        </section>
+                        <section>
+                            <h3>3. Seasons</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ipsam dignissimos quam! Praesentium harum itaque ut labore dicta et ea accusantium voluptatem, veritatis tenetur culpa? Impedit, natus? Odio, tempora unde.</p>
+                        </section>
+                    </TopicCard>
+                </div>
+                <div onClick={() => setActiveIndex(activeIndex === 2 ? 0 : 2)}>
+                    <TopicCard
+                        isActive={activeIndex === 2}
+                        title={"Azimuth"}>
+                        <section>
+                        </section>
+                    </TopicCard>
+                </div>
             </div>
         </div>
     )
