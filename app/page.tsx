fd63801a79c7cee39d76c07fd6c8b40fdb28ca15
Sherwin Burgess <sherwinburgess@gmail.com>
2025-12-02 08:20:28 -0400

    add new home UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,63 @@
 import Image from "next/image"
 import Link from "next/link";
-import Button from "./components/Button";
-import graph_lg from "../public/graph-lg.png"
-import dataImg from "../public/data-sm.svg"
+import crown from "../public/crown.png"
+
 
 export default function Home() {
   return (
     <div className="flex-1 flex flex-col space-y-30 sm:space-y-40 my-20">
-      {/* Hero Section */}
-      <div className='space-y-14 mx-8'>
-        <h1 className="text-5xl/14 sm:text-7xl/24 text-center tracking-wider font-[Space_Grotesk] text-[#F0662A]">
-          Try our solar power calculator
+      <section className='space-y-10 mx-8'>
+        <h1 className="static text-[54px]/16 sm:text-7xl/24 text-center tracking-wider font-[Space_Grotesk] text-[#F6513A]">
+          Master the Power of the Sun
         </h1>
-        <h2 className="text-2xl text-center tracking-wider font-[Space_Grotesk]">
-          The solar power calculator accurately predicts the future power generation of your solar system.
+        <div className="absolute right-0 top-75 mx-6 z-0">
+          <Image src={crown} alt="crown" />
+        </div>
+        <h2 className="text-lg text-center tracking-wide font-[Space_Grotesk] font-light">
+          Use the solar power calculator to accurately predict the future power generation of your solar system.
         </h2>
-        <div className='flex justify-center text-xl font-light font-[Inter]'>
+        <div className='flex justify-center text-lg font-light font-[Inter]'>
           <Link href={"/calculator"}>
-            <Button
-              text={"Try Calculator"}
-              style="bg-linear-[#DD6B19,#F0662A] w-xs" />
+            <button
+              type="button"
+              className="py-4.5 rounded-3xl cursor-pointer tracking-wider bg-linear-[97deg,#F6513A,#F0662A,#F2C521] w-xs">
+              Solar Calculator
+            </button>
           </Link>
         </div>
-      </div>
-      {/* Benefits Section */}
-      <div className='flex flex-col-reverse sm:flex-row mx-8 gap-12 sm:gap-6 justify-evenly items-center'>
-        <ul className="space-y-4 text-xl font-[Inter] font-light list-disc">
+      </section>
+      <section className='mx-auto mx-8 space-y-6'>
+        <h2 className="text-center text-[#F6513A] text-[40px] font-[Darker_Grotesque]">
+          Benefits
+        </h2>
+        <ul className="text-center space-y-2 text-lg font-[Space_Grotesk] font-light list-disc list-inside">
           <li>Accurately predict future power generation</li>
           <li>Optimise your solar system</li>
           <li>Keep a history of past performance</li>
           <li>Analyse future performance</li>
         </ul>
-        <div>
-          <Image src={graph_lg} alt="graph" />
+      </section>
+      <section className='mx-auto mx-8 space-y-6'>
+        <h2 className="text-center text-[#F6513A] text-[40px] font-[Darker_Grotesque]">
+          Summary
+        </h2>
+        <p className="px-20 text-lg text-center font-[Space_Grotesk] font-light">
+          The solar calculator uses the most accurate real
+          time weather data, location data and panel performance
+          data to give an accurate forecast of future power
+          generation of your current or future solar system.
+        </p>
+
+        <div className='flex justify-center text-md font-light font-[Inter]'>
+          <Link href={"/learn"}>
+            <button
+              type="button"
+              className="py-2 border-3 border-[#F6513A] rounded-2xl cursor-pointer tracking-wider w-sm">
+              Learn more
+            </button>
+          </Link>
         </div>
-      </div>
-      {/* Summary section */}
-      <div className='flex flex-col sm:flex-row items-center mx-8 gap-6'>
-        <Image src={dataImg} alt="data" />
-        <div className='text-lg font-[Inter] font-light flex-1 space-y-6'>
-          <p className="px-8 text-xl">
-            The solar calculator uses the most accurate real
-            time weather data, location data and panel performance
-            data to give an accurate forecast of future power
-            generation of your current or future solar system.
-          </p>
-          <div className="flex justify-center text-lg font-light font-[Inter]">
-            <Link href={"/learn"}>
-              <Button
-                text="Learn more"
-                style="bg-linear-[#DD6B19,#F0662A] w-xs" />
-            </Link>
-          </div>
-        </div>
-      </div>
+      </section>
     </div >
   );
 }
