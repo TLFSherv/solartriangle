052880ef5864e66bbff845685b22b10b8c159b40
Sherwin Burgess <sherwinburgess@gmail.com>
2025-12-27 08:43:08 -0400

    moved data manipulation code to its own hook

---
+++ app/dashboard/hooks/usePrepareData.ts
@@ -0,0 +1,70 @@
+import { useMemo } from "react";
+import { poa_monthly, time_daily, gti_daily, poa_hourly } from "../test-data/data";
+
+type Dataset = {
+    x: string[];
+    y: number[];
+    type: 'months' | 'hrs' | 'days';
+}
+export default function usePrepareData(selected: number[]) {
+    const result = useMemo(() => {
+        const getDataByDay = (): Dataset => {
+            let prevDate: Date = new Date(time_daily[0]);
+            let hourlyData: Dataset[] = Array(7).fill({ x: [], y: [], type: 'hrs' });
+            let start = 0;
+            // group data by date
+            for (let i = 0; i < time_daily.length; ++i) {
+                let date = new Date(time_daily[i]);
+                if (date.getUTCDate() !== prevDate.getUTCDate()) {
+                    hourlyData[prevDate.getUTCDay()] = {
+                        x: time_daily.slice(start, i),
+                        y: gti_daily.slice(start, i),
+                        type: 'hrs'
+                    };
+                    start = i;
+                }
+                prevDate = date;
+            }
+            if (selected.length > 1) return hourlyData[selected[1]];
+
+            let dailyData: Dataset = { x: [], y: [], type: "days" };
+            hourlyData.forEach(({ x, y }) => {
+                dailyData.x.push(x[0])
+                const dailyTotal = y.reduce((acc, current) => acc + current)
+                dailyData.y.push(Math.round(dailyTotal))
+            })
+            return dailyData;
+        }
+        const getDataByMonth = (): Dataset => {
+            const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+                "Aug", "Sep", "Oct", "Nov", "Dec"];
+            if (selected.length === 1) return { x: months, y: poa_monthly, type: "months" }
+            // get the number of days in each month of the year
+            const daysPerMonth = new Array(12).fill(0).map((_, index) => new Date(2025, index + 1, 0).getDate())
+            let hourlyPoaByMonth: number[][] = [];
+
+            // parse array to get poa values per month
+            let initialValue = 0, totalHrs = 0;
+            daysPerMonth.forEach(days => {
+                totalHrs += days * 24;
+                hourlyPoaByMonth.push(poa_hourly.slice(initialValue, totalHrs));
+                initialValue = totalHrs;
+            })
+            // sum the poa for each day in each month
+            let dailyPoaByMonth: number[] = [];
+            let dayOfMonth: string[] = [];
+            const m = selected[1];
+            const poaHrs = hourlyPoaByMonth[m];
+            let poaDayTotal = 0;
+            for (let i = 0; i < daysPerMonth[m]; ++i) {
+                poaDayTotal = poaHrs.slice(i * 24, 24 * (i + 1)).reduce((a, b) => a + b);
+                dailyPoaByMonth.push(Math.round(poaDayTotal))
+                dayOfMonth.push(`${months[m]} ${i + 1}`);
+            }
+
+            return { x: dayOfMonth, y: dailyPoaByMonth, type: 'months' };
+        }
+        return (selected[0] === 0) ? getDataByDay() : getDataByMonth()
+    }, [selected.join("-")])
+    return result;
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,10 @@
 "use client"
-import React, { useMemo } from "react";
+import { useState } from "react";
 import useFetchData from "./hooks/useFetchData";
-import { poa_monthly, time_daily, gti_daily, poa_hourly } from "./test-data/data";
+import usePrepareData from "./hooks/usePrepareData";
 import Chart from "./components/Chart";
 import ChartMenu from "./components/ChartMenu";
-import { useState } from "react";
 
-type Dataset = {
-    x: string[];
-    y: number[];
-    type: 'months' | 'hrs' | 'days';
-}
 
 export default function Dashboard() {
     // const result = useFetchData();
@@ -34,84 +28,14 @@ export default function Dashboard() {
     */
 
     const [selected, setSelected] = useState<number[]>([0]);
-    const dataset: Dataset[] = useMemo(() => groupByDate(time_daily), [time_daily]);
-
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
-        "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-    /*
-        1. Number of days in month 
-            month_hours = num_days * 24hr;
-        2. parse array to get poa values per month
-        3. sum the poa for each day in each month
-    */
-    const daysPerMonth = new Array(12).fill(0).map((_, index) => new Date(2025, index + 1, 0).getDate())
-    let hourlyPoaByMonth: { month: number; hourlyPoa: number[] }[] = [];
-
-    let initialValue = 0, totalHrs = 0;
-    daysPerMonth.forEach((days, i) => {
-        totalHrs += days * 24;
-        const hourlyPoa = poa_hourly.slice(initialValue, totalHrs)
-        hourlyPoaByMonth.push({ month: i + 1, hourlyPoa });
-        initialValue = totalHrs;
-    })
-
-    let dailyPoaByMonth: number[] = [];
-    let dayOfMonth: string[] = [];
-    if (selected[0] === 1 && selected.length > 1) {
-        const m = selected[1];
-        const poa = hourlyPoaByMonth[m].hourlyPoa;
-        let poaDayTotal = 0;
-        for (let i = 0; i < daysPerMonth[m]; ++i) {
-            poaDayTotal = poa.slice(i * 24, 24 * (i + 1)).reduce((a, b) => a + b);
-            dailyPoaByMonth.push(Math.round(poaDayTotal))
-            dayOfMonth.push(`${months[m]} ${i + 1}`);
-        }
-    }
-
-    console.log({ x: dayOfMonth, y: dailyPoaByMonth, type: 'days' });
-
-    let gti_weekly: Dataset = { x: [], y: [], type: "days" };
-    dataset.forEach(({ x, y }) => {
-        gti_weekly.x.push(x[0])
-        const gtiDailySum = y.reduce((accumulator, currentValue) => accumulator + currentValue)
-        gti_weekly.y.push(Math.round(gtiDailySum))
-    })
-
-    let selectedDataset: Dataset;
-    if (selected[0] === 0)
-        selectedDataset = (selected.length > 1) ? dataset[selected[1]] : gti_weekly;
-    else {
-        selectedDataset = (selected.length > 1) ? { x: dayOfMonth, y: dailyPoaByMonth, type: 'months' } : { x: months, y: poa_monthly, type: "months" };
-    }
-
-    function groupByDate(timeData: string[]): Dataset[] {
-        let prevDate: Date = new Date(timeData[0]);
-        let dataset: Dataset[] = Array(7).fill({ x: [], y: [], type: 'hrs' });
-        let start = 0;
-
-        for (let i = 0; i < timeData.length; ++i) {
-            let date = new Date(timeData[i]);
-            if (date.getUTCDate() !== prevDate.getUTCDate()) {
-                dataset[prevDate.getUTCDay()] = {
-                    x: time_daily.slice(start, i),
-                    y: gti_daily.slice(start, i),
-                    type: 'hrs'
-                };
-                start = i;
-            }
-            prevDate = date;
-        }
-
-        return dataset;
-    }
+    const dataset = usePrepareData(selected);
 
     return (
         <div className="h-[100px] w-full">
             <ChartMenu
                 selected={selected}
                 setSelected={setSelected} />
-            <Chart dataset={selectedDataset} />
+            <Chart dataset={dataset} />
         </div>
     )
 }
