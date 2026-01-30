0e265e7ac9e465e35230bf22798dde000cd61f17
Sherwin Burgess <sherwinburgess@gmail.com>
2026-01-31 00:19:54 +0100

    use cache data for address, lat and lng on dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,19 +1,19 @@
-import { inputs } from "./test-data/data"
+import { getCachedData } from "@/actions/data"
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    // const dataStr = localStorage.getItem("calculatorData");
-    // const data = JSON.parse(dataStr as string);
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    let { data, success, error } = await getCachedData('calculatorData');
     return (
         <div className="space-y-8">
             <h1 className="text-center pt-10 font-[Space_Grotesk] text-4xl font-light text-[#6E6E6E] tracking-wide">
                 Solar Dashboard
             </h1>
             <div className="flex justify-center">
-                <ol className="space-y-2 font-[Space_Grotesk] font-medium tracking-wide text-gray-400">
-                    <li>Address: <span className="font-light">{inputs.address}</span></li>
-                    <li>Latitude: <span className="font-light">{inputs.lat}</span></li>
-                    <li>Longitude: <span className="font-light">{inputs.lng}</span></li>
-                </ol>
+                {success && (
+                    <ol className="space-y-2 font-[Space_Grotesk] font-medium tracking-wide text-gray-400">
+                        <li>Address: <span className="font-light">{data?.address}</span></li>
+                        <li>Latitude: <span className="font-light">{data?.lat}</span></li>
+                        <li>Longitude: <span className="font-light">{data?.lng}</span></li>
+                    </ol>)}
             </div>
             {children}
         </div>
