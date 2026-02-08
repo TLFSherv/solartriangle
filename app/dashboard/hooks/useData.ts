5e409bdb09d1984b4b24ec359acec3234b6f0b49
Sherwin Burgess <sherwinburgess@gmail.com>
2026-02-08 14:31:38 +0100

    remove useData hook

--- app/dashboard/hooks/useData.ts
+++
@@ -1,8 +0,0 @@
-import useFetchData from "../../../actions/fetchData";
-import useFormatData from "./useFormatData";
-
-export default function useData() {
-    // const data = useFetchData();
-    // console.log(data);
-    const result = useFormatData
-}
