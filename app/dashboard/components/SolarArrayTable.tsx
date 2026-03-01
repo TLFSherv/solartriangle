9e9dc25ac14571c7815e1609a65790e6711f8340
Sherwin Burgess <sherwinburgess@gmail.com>
2026-03-01 02:24:01 +0100

    add horizontal scrolling to table and list on dashboard

--- app/dashboard/components/SolarArrayTable.tsx
+++ app/dashboard/components/SolarArrayTable.tsx
@@ -8,26 +8,30 @@ import { type CalculatorData } from "@/app/types/types";
 export default function SolarArrayTable() {
     const calculatorData: CalculatorData = useContext(CalculatorContext);
     return (
-        <div className="flex flex-row justify-center font-[Darker_Grotesque]">
-            <table className="w-full sm:w-4/5 text-lg sm:text-2xl text-left min-w-max text-gray-300 mx-4">
+        <div className="flex flex-row font-[Darker_Grotesque] overflow-auto whitespace-nowrap w-[90%] mx-auto">
+            <table className="w-full mx-auto sm:w-4/5 text-lg sm:text-2xl text-left min-w-max text-gray-300">
                 <thead>
                     <tr>
-                        <th className="border-b border-blue-gray-50">Name</th>
-                        <th className="border-b border-blue-gray-50">Capacity</th>
-                        <th className="border-b border-blue-gray-50">Quantity</th>
-                        <th className="border-b border-blue-gray-50">Area</th>
-                        <th className="border-b border-blue-gray-50">Azimuth</th>
-                        <th className="border-b border-blue-gray-50">Color</th>
+                        <th className="border-b border-blue-gray-50 px-2">Name</th>
+                        <th className="border-b border-blue-gray-50 px-2">Capacity</th>
+                        <th className="border-b border-blue-gray-50 px-2">Quantity</th>
+                        <th className="border-b border-blue-gray-50 px-2">Area</th>
+                        <th className="border-b border-blue-gray-50 px-2">Azimuth</th>
+                        <th className="border-b border-blue-gray-50 px-2">Tilt</th>
+                        <th className="border-b border-blue-gray-50 px-2">Losses</th>
+                        <th className="border-b border-blue-gray-50 px-2">Color</th>
                     </tr>
                 </thead>
                 <tbody className="font-light">
                     {calculatorData.solarArrays.map((d, i) => {
                         return (<tr key={i}>
-                            <td className="border-b border-blue-gray-50">Array {d.id}</td>
-                            <td className="border-b border-blue-gray-50">{d.capacity}</td>
-                            <td className="border-b border-blue-gray-50">{d.quantity}</td>
-                            <td className="border-b border-blue-gray-50">{d.area}</td>
-                            <td className="border-b border-blue-gray-50">{d.azimuth}</td>
+                            <td className="border-b border-blue-gray-50 px-2">Array {d.id}</td>
+                            <td className="border-b border-blue-gray-50 px-2">{d.capacity}</td>
+                            <td className="border-b border-blue-gray-50 px-2">{d.quantity}</td>
+                            <td className="border-b border-blue-gray-50 px-2">{d.area}</td>
+                            <td className="border-b border-blue-gray-50 px-2">{d.azimuth}</td>
+                            <td className="border-b border-blue-gray-50 px-2">{d.tilt}</td>
+                            <td className="border-b border-blue-gray-50 px-2">{d.losses}</td>
                             <td style={{ backgroundColor: colors[i] }} className='border-b border-blue-gray-50'></td>
                         </tr>)
                     })}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,13 +8,13 @@ export default async function DashboardLayout({ children }: { children: React.Re
         return
     }
     return (
-        <div className="space-y-8 sm:space-y-16 mt-10 sm:mt-16 text-[#F2F2F0]">
+        <div className="space-y-8 sm:space-y-16 mt-8 sm:mt-16 text-[#F2F2F0]">
             <div className="flex flex-col items-center font-[Space_Grotesk]">
                 <div className="mx-auto"><Toaster /></div>
                 {(cacheResult.data &&
-                    <ol className="font-[Darker_Grotesque] font-medium tracking-wide text-gray-300 text-lg sm:text-xl md:text-2xl">
+                    <ol className="w-1/2 mx-auto font-[Darker_Grotesque] font-medium tracking-wide text-gray-300 text-lg sm:text-xl md:text-2xl">
                         <li>Country: <span className="font-light">{cacheResult.data.location.country}</span></li>
-                        <li>Address: <span className="font-light">{cacheResult.data.location.address}</span></li>
+                        <li className="overflow-auto whitespace-nowrap ">Address: <span className="font-light">{cacheResult.data.location.address}</span></li>
                         <li>Time zone: <span className="font-light">{cacheResult.data.location.timeZone}</span></li>
                     </ol>)}
             </div>
